54f98dfd22704e80628c8bd8a8381faff415e9a5
dunyanong <ong.dy2004@gmail.com>
2023-04-08 01:17:46 +0800

    infinite commit

--- prisma/migrations/20230407155456_migration/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Note" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "date" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
