9d1e3441b0d5137393f73e8451601f5507c6cfe7
dunyanong <ong.dy2004@gmail.com>
2023-04-07 23:56:27 +0800

    added new db

---
+++ prisma/migrations/20230407155456_migration/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
